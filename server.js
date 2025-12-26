6b18d043d5d7256b49f03e1b672d456c8e6b9748
Wz <jaxksonlau361@gmail.com>
2025-12-26 17:28:24 +0800

    delete listen

--- server.js
+++ server.js
@@ -68,10 +68,6 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
